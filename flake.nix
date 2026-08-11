5b41ccc409a606fc2b61467b9f0198ad31786b34
Ivan D Vasin <ivan@nisavid.io>
2026-08-11 08:52:38 -0400

    fix(nix): remove stale Cargo output hash

--- flake.nix
+++ flake.nix
@@ -313,9 +313,6 @@
 
           cargoLock = {
             lockFile = ./Cargo.lock;
-            outputHashes = {
-              "cosmic-protocols-0.2.0" = "sha256-ymn+BUTTzyHquPn4hvuoA3y1owFj8LVrmsPu2cdkFQ8=";
-            };
           };
 
           buildAndTestSubdir = "read-aloud-linux";
